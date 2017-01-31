ab8fab4bda725fdea05f89cc59a07b3b7b058a66
Steve <sbarry50@gmail.com>
2017-01-31 13:38:01 -0500

    built HTML view files

---
+++ src/shortcode/views/qa.php
@@ -0,0 +1,11 @@
+<dl class="qa--container">
+  <dt class="qa--question" itemscope itemtype="http://schema.org/Question" data-show-icon="<?php echo $attributes['show_icon']; ?>" data-hide-icon="<?php esc_attr_e( $attributes['hide_icon'] ); ?>">
+      <span class="<?php echo $attributes['show_icon']; ?>" aria-hidden="true">
+          <span class+"screen-reader-text">Click to reveal the answer</span>
+      </span>
+      <?php esc_html_e( $attributes['question'] ); ?>
+  </dt>
+  <dl class="qa--answer" itemprop="suggestedAnswer" itemscope itemtype="http://schema.org/Answer" style="display: none;">
+      <?php echo $hidden_content; ?>
+  </dl>
+</dl>
---
+++ src/shortcode/views/teaser.php
@@ -0,0 +1,11 @@
+<div class="teaser--container" itemscope itemtype="http://schema.org/BlogPosting">
+  <div class="teaser--visible-message" itemprop="headline"  data-show-icon="<?php echo $attributes['show_icon']; ?>" data-hide-icon="<?php esc_attr_e( $attributes['hide_icon'] ); ?>">
+      <span class="<?php echo $attributes['show_icon']; ?>" aria-hidden="true">
+          <span class+"screen-reader-text">Click to reveal the message</span>
+      </span>
+      <?php esc_html_e( $attributes['visible_message'] ); ?>
+  </div>
+  <div class="teaser--hidden-content" itemprop="description" itemscope itemtype="http://schema.org/Answer" style="display: none;">
+      <?php echo $hidden_content; ?>
+  </div>
+</div>
